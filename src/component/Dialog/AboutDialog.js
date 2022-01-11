a651d4194b9c701c825fda0dfc5269e0f4258a1f
whaoa <whao.w@outlook.com>
2022-01-11 22:10:08 +0800

    feat: 修改项目说明

--- src/component/Dialog/AboutDialog.js
+++ src/component/Dialog/AboutDialog.js
@@ -28,9 +28,6 @@ class AboutDialog extends Component {
         onOk={this.handleOk}
         onCancel={this.handleCancel}
         footer={[
-          <Button key="version" onClick={this.handleVersion}>
-            更新记录
-          </Button>,
           <Button key="submit" type="primary" onClick={this.handleOk}>
             确认
           </Button>,
@@ -52,56 +49,30 @@ class AboutDialog extends Component {
           </a>
         </h3>
 
-        <p style={style.lineHeight}>支持自定义样式的 Markdown 编辑器；</p>
-        <p style={style.lineHeight}>支持微信公众号、知乎和稀土掘金；</p>
-        <h3 style={style.headerMargin}>我们</h3>
         <p style={style.lineHeight}>
-          如果你喜欢我们的工具，欢迎关注
+          本项目 fork 自
           <a
             id="nice-about-dialog-github"
             rel="noopener noreferrer"
             target="_blank"
             href="https://github.com/mdnice/markdown-nice"
           >
-            &nbsp;GitHub&nbsp;
+            &nbsp;Markdown Nice&nbsp;
           </a>
-          、
-          <a
-            id="nice-about-dialog-gitee"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://gitee.com/zhning12/markdown-nice"
-          >
-            &nbsp;Gitee&nbsp;
-          </a>
-          和新项目
-          <a
-            id="nice-about-dialog-resume"
-            style={style.a}
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/guanpengchn/markdown-resume"
-          >
-            &nbsp;Markdown 简历
-          </a>
-          ，同时我们也组建了「
-          <strong style={style.strong}>微信群</strong>
-          」，右下角关注公众号回复「排版」拉你入群，愿意
-          <strong style={style.strong}>微信打赏</strong>
-          就更好啦。
+          ，基于 [master 分支的 8f5205c] 进行的二次修改。
         </p>
-        <div style={{display: "flex", alignItems: "center"}}>
-          <img
-            alt="图片描述"
-            style={style.leftImgWidth}
-            src="https://my-wechat.mdnice.com/mdnice/bonus_20191007150639.png"
-          />
-          <img
-            alt="图片描述"
-            style={style.rightImgWidth}
-            src="https://imgkr.cn-bj.ufileos.com/22cf98bd-3f85-45fc-9df7-e6b2808329d0.png"
-          />
-        </div>
+        <p style={style.lineHeight}>支持自定义样式的 Markdown 编辑器；</p>
+        <p style={style.lineHeight}>支持微信公众号、知乎和稀土掘金；</p>
+        <p style={style.lineHeight}>
+          <a
+            id="nice-about-dialog-github"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/whaoa/markdown-nice"
+          >
+            &nbsp;本项目 GitHub 仓库&nbsp;
+          </a>
+        </p>
       </Modal>
     );
   }
